fb1371d8b8eb95bbf88fa3a7a45c7a0c40111508
zeeshansalmani <salmanizeeshan>
2022-07-05 21:08:12 +0530

    function

--- firstrepo.ts
+++ firstrepo.ts
@@ -1,6 +1,12 @@
 function add(x:number,y:number):number{
     return x+y
 }
-function subtract(x: number,y:number):number{
+function subtract(x:number,y:number):number{
     return x-y
+}
+function multiply(x:number,y:number):number{
+    return x*y
+}
+function divide(x:number,y:number):number{
+    return x/y
 }
