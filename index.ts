5cf9712b82fb5feac88b41984c6c0856ea958b05
zeeshansalmani <salmanizeeshan027@gmail.com>
2022-07-05 19:04:23 +0530

    i added

--- index.ts
+++ index.ts
@@ -1,13 +1,3 @@
 function add(x: number,y:number):number{
     return x+y
 }
-function subtract(x: number,y:number):number{
-    return x-y
-}
-
-function multiply(x: number,y:number):number{
-    return x * y
-}
-function divide(x: number,y:number):number{
-    return x / y
-}
